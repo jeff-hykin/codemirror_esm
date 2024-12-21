588d10c26b913653ee9a5d9b6bf57bfacefd2673
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-21 12:15:28 -0500

    update themes

--- themes/abcdef.js
+++ themes/abcdef.js
@@ -1,7 +1,7 @@
 
 // https://cdn.jsdelivr.net/npm/@uiw/codemirror-theme-abcdef@4.23.6/esm/index.js
 
-export var defaultSettings = {
+export const defaultSettings = {
   background: '#0f0f0f',
   foreground: '#defdef',
   caret: '#00FF00',
@@ -11,8 +11,50 @@ export var defaultSettings = {
   gutterForeground: '#FFFFFF',
   lineHighlight: '#0a6bcb3d'
 }
-export default ({ variant="dark", settings={}, styles=[], mutateThemeStyles=(style)=>style }={}) => tags => {
+/**
+ * Instantiate a CodeMirror theme
+ * 
+ * @example
+ * ```js
+ * const themeConfig = theme({
+ *   settings: {
+ *     background: '#272C35',
+ *     foreground: '#9d9b97',
+ *     caret: '#797977',
+ *     selection: '#3d4c64',
+ *     selectionMatch: '#3d4c64',
+ *     gutterBackground: '#272C35',
+ *     gutterForeground: '#465063',
+ *     gutterBorder: 'transparent',
+ *     lineHighlight: '#2e3f5940'
+ *   },
+ *   // effectively delete the existing red styles
+ *   mutateThemeStyles: (style, tags) => (style.color == 'red')?null:style,
+ *   // add some new styles
+ *   styles: (tags) => [
+ *     {
+ *       tag: [t.tagName, t.heading],
+ *       color: '#e06c75',
+ *     }, {
+ *       tag: [t.function(t.variableName), t.function(t.propertyName), t.url, t.processingInstruction],
+ *       color: 'hsl(207, 82%, 66%)',
+ *     },
+ *   ],
+ * });
+ * ```
+ * @param {Object} [config={}] Configuration object
+ * @param {Object} [config.settings={}] Override or add to the default theme settings
+ * @param {Function|Array} [config.styles=[]] A function that takes `tags` as an argument and returns an array of styles. Can also be an array of functions, each of which takes a `tags` argument and returns a style (for composability).
+ * @param {Function} [config.mutateThemeStyles=(style, tags)=>style] A function that can modify the theme styles, returning `null` will remove the style.
+ * @param {string} [config.variantOverride="dark"] Note: only some themes support variants. For most this will simply change what variant the editor considers the theme to be.
+ * 
+ * @returns {Function} a function that takes `tags` as an argument, so that it can be used with any version of @lezer/highlight
+ */
+export const theme = ({ settings={}, styles=[], mutateThemeStyles=(style, tags)=>style, variantOverride="dark", }={}) => tags => {
     const t = tags
+    if (styles instanceof Function) {
+        styles = styles(t)
+    }
     const style = [
         {
           tag: t.keyword,
@@ -67,10 +109,11 @@ export default ({ variant="dark", settings={}, styles=[], mutateThemeStyles=(sty
           color: 'blueviolet',
           fontWeight: 'bold'
         }
-    ].map(mutateThemeStyles).filter(each=>each)
+    ].map((each)=>mutateThemeStyles(each, t)).filter(each=>each)
     return {
-        variant,
+        variant: variantOverride,
         settings: Object.assign({}, defaultSettings, settings),
-        styles: [...style, ...styles],
+        styles: [...style, ...styles.map(each=>(each instanceof Function)?each(t):each)],
     }
 }
+export default theme
--- themes/material/light.js
+++ themes/material/light.js
@@ -1,7 +1,7 @@
 
 // https://cdn.jsdelivr.net/npm/@uiw/codemirror-theme-material@4.23.6/esm/light.js
 
-export var defaultSettings = {
+export const defaultSettings = {
   background: '#2e3235',
   foreground: '#bdbdbd',
   caret: '#a0a4ae',
@@ -12,8 +12,50 @@ export var defaultSettings = {
   gutterActiveForeground: '#4f5b66',
   lineHighlight: '#545b6130'
 }
-export default ({ variant="dark", settings={}, styles=[], mutateThemeStyles=(style)=>style }={}) => tags => {
+/**
+ * Instantiate a CodeMirror theme
+ * 
+ * @example
+ * ```js
+ * const themeConfig = theme({
+ *   settings: {
+ *     background: '#272C35',
+ *     foreground: '#9d9b97',
+ *     caret: '#797977',
+ *     selection: '#3d4c64',
+ *     selectionMatch: '#3d4c64',
+ *     gutterBackground: '#272C35',
+ *     gutterForeground: '#465063',
+ *     gutterBorder: 'transparent',
+ *     lineHighlight: '#2e3f5940'
+ *   },
+ *   // effectively delete the existing red styles
+ *   mutateThemeStyles: (style, tags) => (style.color == 'red')?null:style,
+ *   // add some new styles
+ *   styles: (tags) => [
+ *     {
+ *       tag: [t.tagName, t.heading],
+ *       color: '#e06c75',
+ *     }, {
+ *       tag: [t.function(t.variableName), t.function(t.propertyName), t.url, t.processingInstruction],
+ *       color: 'hsl(207, 82%, 66%)',
+ *     },
+ *   ],
+ * });
+ * ```
+ * @param {Object} [config={}] Configuration object
+ * @param {Object} [config.settings={}] Override or add to the default theme settings
+ * @param {Function|Array} [config.styles=[]] A function that takes `tags` as an argument and returns an array of styles. Can also be an array of functions, each of which takes a `tags` argument and returns a style (for composability).
+ * @param {Function} [config.mutateThemeStyles=(style, tags)=>style] A function that can modify the theme styles, returning `null` will remove the style.
+ * @param {string} [config.variantOverride="dark"] Note: only some themes support variants. For most this will simply change what variant the editor considers the theme to be.
+ * 
+ * @returns {Function} a function that takes `tags` as an argument, so that it can be used with any version of @lezer/highlight
+ */
+export const theme = ({ settings={}, styles=[], mutateThemeStyles=(style, tags)=>style, variantOverride="light", }={}) => tags => {
     const t = tags
+    if (styles instanceof Function) {
+        styles = styles(t)
+    }
     const style = [
         {
           tag: t.keyword,
@@ -133,10 +175,11 @@ export default ({ variant="dark", settings={}, styles=[], mutateThemeStyles=(sty
           color: '#606f7a',
           borderBottom: "1px dotted #ff5f52"
         }
-    ].map(mutateThemeStyles).filter(each=>each)
+    ].map((each)=>mutateThemeStyles(each, t)).filter(each=>each)
     return {
-        variant,
+        variant: variantOverride,
         settings: Object.assign({}, defaultSettings, settings),
-        styles: [...style, ...styles],
+        styles: [...style, ...styles.map(each=>(each instanceof Function)?each(t):each)],
     }
 }
+export default theme
