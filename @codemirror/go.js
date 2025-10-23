4cc6ded5e2f14dd3531a6dd8ff79fccbee5c366f
Jeff Hykin <jeff.hykin@gmail.com>
2025-10-23 15:46:55 -0500

    generate

--- @codemirror/go.js
+++
@@ -1,3 +0,0 @@
-import { StreamLanguage } from "./language.js"
-import {go as _go} from "../vendored/esm.sh/@codemirror/legacy-modes/mode/go.js"
-export const go = StreamLanguage.define(_go)
--- @codemirror/pegjs.js
+++
@@ -1,3 +0,0 @@
-import { StreamLanguage } from "./language.js"
-import {pegjs as _pegjs} from "../vendored/esm.sh/@codemirror/legacy-modes/mode/pegjs.js"
-export const pegjs = StreamLanguage.define(_pegjs)
